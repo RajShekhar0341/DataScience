1837836161b9f8a1d7c1c83eed7545c2292633fc
RajShekhar0341 <rajshekharbesant@gmail.com>
2026-03-14 07:58:07 +0000

    prog15

---
+++ College files/currentthread.java
@@ -0,0 +1,8 @@
+public class currentthread {
+    public static void main(String[] args) {
+        // Get the reference to the current thread
+        Thread t = Thread.currentThread();
+        // Print the reference
+        System.out.println("Current thread: " + t);
+    }
+}
