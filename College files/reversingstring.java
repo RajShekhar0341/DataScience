0a3dd1b5e1b47fdb2a3f51e8b59ef52928a956e5
RajShekhar0341 <rajshekharbesant@gmail.com>
2026-03-16 03:14:16 +0000

    Add reversing string functionality with main method

---
+++ College files/reversingstring.java
@@ -0,0 +1,13 @@
+public class reversingstring {
+    public static void main(String[] args) {
+        String original = "Hello, World!";
+        String reversed = reverseString(original);
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
+
+    public static String reverseString(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
+    }
+}
